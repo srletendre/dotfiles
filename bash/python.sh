e41408d91fcc2631b3d941e6fce3832324f041ff
srletendre <seanletendre@gmail.com>
2026-02-17 09:31:29 -0500

    Create python.sh

---
+++ bash/python.sh
@@ -0,0 +1,7 @@
+alias p3=python3
+alias p311='python3.11'
+alias ipy=ipython
+alias bpy=bpython
+alias dact=deactivate # leave a venv
+export VIRTUAL_ENV_DISABLE_PROMPT=1 # we add the venv name a different way to the prompt
+export PYTHONSTARTUP='~/python_startup.py'
